6f824c6a75432f2f96ae09b72c7e6a388b6a372c
Edwin Corea Bonilla <edwin.corea@gmail.com>
2015-09-01 16:11:50 -0600

    .

--- app/js/services/books.js
+++
@@ -1,9 +0,0 @@
-app.factory('books', ['$http', function($http) {
-    return $http.get('js/services/books.json')
-        .success(function(data) {
-            return data;
-        })
-        .error(function(data) {
-            return data;
-        });
-}]);
